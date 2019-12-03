49011624f2cc7abe957fe6e38d04064e4b26fbc4
NataliaGodot <natalia_godot@hotmail.com>
2019-12-03 14:38:37 -0300

    Add cache to Apps client

--- node/index.ts
+++ node/index.ts
@@ -1,20 +1,34 @@
-import { ClientsConfig, method, Service } from '@vtex/api'
+import { ClientsConfig, LRUCache, method, Service } from '@vtex/api'
 
-import { Clients } from './clients/index'
+import { Clients } from './clients'
 import { unwrap } from './events/crowdinAPI/unwrap'
 import { updateCrowdinProject } from './events/crowdinAPI/update'
 import { saveInCrowdin } from './events/export'
 import { getSettings } from './events/settings'
-import { logUpdateInTranslations } from './middlewares/listenToUpdates'
+import { getInfosFromCrowdin } from './middlewares/listenToUpdates'
 import { saveIOMessage } from './middlewares/saveUpdateInMessages'
 
 const TIMEOUT_MS = 3000
 const TRANSLATION_CONCURRENCY = 5
 const TRANSLATION_RETRIES = 3
+const SHORT_TIMEOUT_MS = 1 * 1000
+const CONCURRENCY = 10
+
+const appsCacheStorage = new LRUCache<string, any>({
+  max: 10,
+})
+
+metrics.trackCache('apps', appsCacheStorage)
 
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
   options: {
+    apps: {
+      concurrency: CONCURRENCY,
+      memoryCache: appsCacheStorage,
+      retries: 1,
+      timeout: SHORT_TIMEOUT_MS,
+    },
     default: {
       retries: 2,
       timeout: TIMEOUT_MS,
@@ -35,7 +49,7 @@ export default new Service<Clients, State>({
   },
   routes: {
     listenToUpdates: method({
-      POST: [getSettings, logUpdateInTranslations, saveIOMessage],
+      POST: [getSettings, getInfosFromCrowdin, saveIOMessage],
     }),
   },
 })
